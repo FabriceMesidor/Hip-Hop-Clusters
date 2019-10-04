41b43316347fcfd10215e4f1d603e3c880819605
Fabrice Mesidor <fmesidor@me.com>
2019-10-04 00:03:33 -0400

    scraping artists from ranker

---
+++ Data-Collection/Lyrics/credentials 2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 17:05:07 2019
+
+@author: fabricemesidor
+"""
+
+# Genius token
+client_id = "Q53fYZR3PDQwVOePYcvxwg6VwWvcrNyc6x2vtbkmY17UxCDumQXdW231ZvMwDDLP"
+api_key = "uQsB27xuJNJjdZaXzxQZSDe9nUsCmF1Wg5rse88OSO0KYROZbhaBrheo60vC3ZedPxuq1ajURIb1mKs7nEionw"
+access_token = "GMDZNVgU2OcDjuRLzboZObyAhQH5j1ezWUX238sZe_vpvOtsEpIoT92WvupiYYAU"
